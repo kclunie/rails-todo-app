303b8223029a8ab84a5bba74e1fdff9696680e44
Kristen Clunie <kcluniesa@yahoo.com>
2020-10-27 17:23:16 -0400

    add card association

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,24 @@
 class Card < ApplicationRecord
     has_many :lists
+
+    def palindrome(string)
+        space = string.gsub(/\s+/, "")
+      
+      i = 0
+        a = []
+        while !space[i..-1].empty?
+           j = -1
+         while !space[i..j].empty?
+           s = space[i..j]
+           if s.reverse == s
+             a << s
+           end
+          j -= 1
+         end
+         i += 1
+       end
+       return a.max_by(&:length)
+      
+      end
+
 end
